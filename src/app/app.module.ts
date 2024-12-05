9268a1165bc845e2c30330eb169f31b82ca47e32
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-12-06 01:20:44 +0200

    generating needed components for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AddNewComponent } from './add-new/add-new.component';
+import { ContactComponent } from './contact/contact.component';
+import { AllNewsComponent } from './all-news/all-news.component';
+import { EditArticleComponent } from './edit-article/edit-article.component';
+import { DetailsArticleComponent } from './details-article/details-article.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NotFound404Component } from './not-found-404/not-found-404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    AddNewComponent,
+    ContactComponent,
+    AllNewsComponent,
+    EditArticleComponent,
+    DetailsArticleComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
