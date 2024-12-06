222f951ff6cc1791c6d0e23f9886f5a95513b250
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-12-06 02:19:47 +0200

    Add assets, fix routing, complete footer and header. Initial preparation of home page, about page, contact page and 404 page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AllNewsComponent } from './all-news/all-news.component';
+import { DetailsArticleComponent } from './/details-article/details-article.component';
+import { EditArticleComponent } from './edit-article/edit-article.component';
+import { AboutUsComponent } from './about-us/about-us.component';  
+import { ContactComponent } from './contact/contact.component';  
+import { NotFound404Component } from './not-found-404/not-found-404.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'all-news', component: AllNewsComponent },
+  // { path: 'hardware', component: HardwareComponent },
+  // { path: 'software', component: SoftwareComponent },
+  { path: 'news/:id', component: DetailsArticleComponent },
+  { path: 'edit/:id', component: EditArticleComponent },
+  { path: 'about', component: AboutUsComponent },  // 
+  { path: 'contact', component: ContactComponent },  
+  { path: '**', component: NotFound404Component },  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
