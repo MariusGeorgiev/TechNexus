94586f1cbba4f646e58682a7e4cfa145dcd6208f
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-12-09 03:59:43 +0200

    page for software is update to fetch data proper way

--- src/app/software-articles/software-articles.component.ts
+++ src/app/software-articles/software-articles.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../services/article.service';
 
 @Component({
   selector: 'app-software-articles',
   templateUrl: './software-articles.component.html',
   styleUrls: ['./software-articles.component.css']
 })
-export class SoftwareArticlesComponent {
+export class SoftwareArticlesComponent implements OnInit {
+  articles: any[] = [];
 
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    // Fetch all articles when the component is initialized
+    this.articleService.getAllArticles().subscribe(data => {
+
+      // Filter articles to includes only software
+      this.articles = data.filter(article => article.category === 'Software');
+    });
+  }
 }
