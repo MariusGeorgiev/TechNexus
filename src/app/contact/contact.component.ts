33208d648e1de4d5c97f5a31841884537579b3fe
Marius Georgiev <117039830+MariusGeorgiev@users.noreply.github.com>
2024-12-15 23:49:09 +0200

    update Contact to use EmailJs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { AuthService } from '../services/auth.service'; 
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-  onSubmit(event: Event): void {
-    event.preventDefault();
-    console.log('Form submitted');
-    alert('Message sent successfully!');
+
+export class ContactComponent implements OnInit {
+  successMessage: string = '';
+  errorMessage: string = '';
+  userEmail: string = ''; 
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const email = this.authService.getUserEmail();
+    if (email) {
+      this.userEmail = email;
+    }
+  }
+  
+
+  sendEmail(event: Event) {
+    event.preventDefault(); 
+
+    const form = event.target as HTMLFormElement;
+
+    const emailParams = {
+      from_name: form['from_name'].value,  
+      from_email: this.userEmail,  
+      message: form['message'].value
+    };
+
+    emailjs.send(
+      'service_qauxr42',   
+      'template_szlnkrm',  
+      emailParams,
+      'ta0FelLgIvC8hcghh' 
+
+    ).then(
+      (response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        this.successMessage = 'Your message has been sent successfully!';
+        form.reset(); 
+      },
+      (error) => {
+        console.error('FAILED...', error);
+        this.errorMessage = 'An error occurred while sending your message. Please try again.';
+      }
+    );
   }
 }
+
